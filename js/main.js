239be9719bdd86dd69e9ec0f1129923a592afecc
Dmitriy <dmitriy.degilev@gmail.com>
2019-06-28 14:34:42 +0300

    create project Casio

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+
+$('.top__slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: true
+});
+
+
+$('.bottom__slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: true
+});
+
+
+$(document).ready(function(){
+	$(".top__to-go").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 900);
+	});
+});
+});
